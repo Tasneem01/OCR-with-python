78309e5b63900387167e96ed96012e844fb1a190
Tasneem01 <76658093+Tasneem01@users.noreply.github.com>
2022-10-14 21:37:45 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+# test_pytesseract.py
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Peter\Anaconda3\envs\tesseract\Library\bin\tesseract.exe'
+
+text_from_image = pytesseract.image_to_string(Image.open('test_image.png'))
+print(text_from_image)
